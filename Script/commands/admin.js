51c4f59f951a573b841c59e102a5d90a22081bc6
SHOWROB-143 <136898413+SHOWROB-143@users.noreply.github.com>
2025-08-04 01:00:53 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -37,8 +37,8 @@ module.exports.run = async function({ api, event }) {
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
         `,
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+        attachment: fs.createReadStream(__dirname + "/cache/1754246382483.jpg")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1754246382483.jpg"));
   
     return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
